1cc4745ad0988cd0303f051d3b94c351c33bfb4a
Ricki-h <162235476+Ricki-h@users.noreply.github.com>
2025-02-28 14:18:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: 'https://orvapi.netlify.app',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
